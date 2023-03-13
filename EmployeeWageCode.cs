39877bdef89b6d6e6f2b8eae44339b1c6a85f5ce
Aakankshap16 <aakankshap971@gmail.com>
2023-03-13 12:57:20 +0530

    fulltym and halftym check

--- EmployeeWageCode.cs
+++ EmployeeWageCode.cs
@@ -9,6 +9,7 @@ namespace EmployeeWageComputation
     internal class EmployeeWageCode
     {
         public const int is_Full_Time = 1;
+        public const int is_Part_Time = 2;
         public const int EmpRatePerHr = 20;
         public static void EmployeeAttendence()
         {
@@ -17,10 +18,14 @@ namespace EmployeeWageComputation
             int empWage = 0;
             int empHrs = 0;
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
             if (empCheck == is_Full_Time)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == is_Part_Time)
+            {
+                empHrs = 4;
             }
             else
             {
