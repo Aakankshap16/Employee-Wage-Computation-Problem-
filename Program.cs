9211640c31732a11ad4a47ea9d992df6955c850f
Aakankshap16 <aakankshap971@gmail.com>
2023-03-13 14:11:36 +0530

    same for all

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         public static void Main(string[] args) 
         {
+            //same for all use cases
             Console.WriteLine("Welcome to the employee wage computation problem!");
             EmployeeWageCode.EmployeeAttendence();
         }
