3c4add4e492d371add6165b602fb0702e2783327
Aakankshap16 <aakankshap971@gmail.com>
2023-03-13 14:18:12 +0530

    same

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@
     {
         public static void Main(string[] args) 
         {
+            // single class use only extenstion in each branch
             Console.WriteLine("Welcome to the employee wage computation problem!");
             EmployeeWageCode.EmployeeAttendence();
+
         }
     }
     
